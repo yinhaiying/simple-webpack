f2dad430ba075dbe6fda6ee5efa02021298a7ed8
yinhaiying <yinhaiying@58.com>
2020-11-09 15:40:13 +0800

    将let转化成vat

--- example/let2var/let_to_var.ts
+++ example/let2var/let_to_var.ts
@@ -7,6 +7,24 @@ const code = `let a = "let";let b = 2`;
 
 // 把代码变成AST
 const ast = parse(code,{sourceType:"module"});
-console.log("ast:",ast);
 
 // 遍历AST
+traverse(ast,{
+    // enter钩子函数表示每进入一个节点的时候就执行这个函数,item是每个节点的信息
+    enter:item => {
+      if(item.node.type === "VariableDeclaration"){
+         if(item.node.kind === "let"){
+           item.node.kind = "var";
+         } 
+      }
+    }
+})
+
+// 将AST生成code2
+const output = generate(
+    ast,
+    {},
+    code
+)
+console.log(output.code)
+console.log(output.map)
